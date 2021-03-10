41ea05a68139a7bb996120da4b6f1f94c5221b37
Mazen Ammar <mazen.e.ammar@gmail.com>
2021-03-09 19:43:03 -0800

    Removed unused change account dialog (#24)

--- app/src/main/java/com/example/cs65_final_project/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/cs65_final_project/fragments/SettingsFragment.java
@@ -3,40 +3,23 @@ package com.example.cs65_final_project.fragments;
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.example.cs65_final_project.FirebaseAuthHelper;
 import com.example.cs65_final_project.R;
 import com.example.cs65_final_project.activities.MainActivity;
-import com.google.firebase.auth.FirebaseAuth;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
-
-    public static final String ACCOUNT_DIALOG_TAG = "account dialog tag";
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey);
 
-        Preference accounts = findPreference("Accounts");
-        accounts.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            @Override
-            public boolean onPreferenceClick(Preference preference) {
-                SwitchAccountsDialog dialog = new SwitchAccountsDialog();
-                dialog.show(getParentFragmentManager(), ACCOUNT_DIALOG_TAG);
-                return false;
-            }
-        });
-        Preference logout = findPreference("Logout");
-        logout.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            @Override
-            public boolean onPreferenceClick(Preference preference) {
-                FirebaseAuthHelper.logout();
-                Intent intent = new Intent(getActivity(), MainActivity.class);
-                startActivity(intent);
-                return false;
-            }
+        findPreference("Logout").setOnPreferenceClickListener(preference -> {
+            FirebaseAuthHelper.logout();
+            Intent intent = new Intent(getActivity(), MainActivity.class);
+            startActivity(intent);
+            return false;
         });
     }
 }
